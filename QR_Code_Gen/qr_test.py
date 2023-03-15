8b37ddbe4f0331d769da7cf4bed02167d8971178
Sergio_lattke <bacuchito@gmail.com>
2023-03-15 19:32:42 -0400

    created a Tests for qr_generator

--- QR_Code_Gen/qr_test.py
+++ QR_Code_Gen/qr_test.py
@@ -1,29 +1,23 @@
-# Importar el módulo unittest y el script Qr_generator.py
-import unittest
-import Qr_generator
 import os
-import qrcode
-from PIL import Image
-# Crear una clase de prueba que herede de unittest.TestCase
-class TestQrGenerator(unittest.TestCase):
+import unittest
+from urllib.parse import urlparse
+from Qr_generator import crear_qr
 
-    # Definir métodos de prueba que comprueben el funcionamiento del código QR
-    def test_folder_creation(self):
-        # Comprobar que se crea la carpeta 'QR_image'
-        self.assertTrue(os.path.exists('QR_image'))
+class TestQRGenerator(unittest.TestCase):
+    
+    def test_crear_qr(self):
+        urls = ["https://www.google.com", "https://www.facebook.com", "https://www.twitter.com"]
+        crear_qr(urls)
+        self.assertTrue(os.path.exists("qr_image/www.google.com.png"))
+        self.assertTrue(os.path.exists("qr_image/www.facebook.com.png"))
+        self.assertTrue(os.path.exists("qr_image/www.twitter.com.png"))
 
-    def test_qr_generation(self):
-        # Comprobar que se generan los archivos QR para cada URL
-        for url in Qr_generator.urls:
-            self.assertTrue(os.path.exists(os.path.join('QR_image', url.replace('https://', '') + '.png')))
-
-    def test_qr_data(self):
-        # Comprobar que los datos del código QR coinciden con la URL
-        for url in Qr_generator.urls:
-            img = Image.open(os.path.join('QR_image', url.replace('https://', '') + '.png'))
-            qr = qrcode.image.pil.PilImage(img, width=10, box_size=4)
-            self.assertEqual(qr.data, url)
-
-# Ejecutar el script de prueba con unittest.main()
+    def tearDown(self):
+        if os.path.exists("qr_image"):
+            for archivo in os.listdir("qr_image"):
+                archivo_path = os.path.join("qr_image", archivo)
+                os.remove(archivo_path)
+            os.rmdir("qr_image")
+        
 if __name__ == '__main__':
     unittest.main()
